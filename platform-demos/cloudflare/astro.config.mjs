d4ba236b05a475d0bf319ad839dbb5abc9cf32d4
Alexandr Studio <alexandr.studio@pm.me>
2025-06-04 18:46:41 +0200

    add: cloudflace demo

---
+++ platform-demos/cloudflare/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+import maintenance from "astro-maintenance";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+
+  integrations: [
+    maintenance({
+      template: "construction",
+      title: "Astro Maintenance on Cloudflare",
+      description:
+        "Testing the Astro Maintenance page version 2.0.0-next.3 on Cloudflare. The override key is 'astro'. Try it out by adding `?astro` to the URL. ",
+      socials: {
+        github: "https://github.com/alexandr-studio/astro-maintenance",
+      },
+      override: "astro",
+    }),
+  ],
+});
